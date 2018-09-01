30c97970f15c1c8222d824a85d7c76d37841684f
Guille Abella <42720588+gabella87@users.noreply.github.com>
2018-09-01 15:31:03 -0300

    v4

---
+++ src/partials/template.php
@@ -0,0 +1,32 @@
+<div class="hello">
+  
+  Hello from <b> GUILLE PLUGIN</b>
+
+  <form>
+    Fecha:<br>
+    <input type="date" name="fecha"><br>
+    Usage idle:<br>
+    <input type="text" name="usage">
+  </form>
+
+  <?php
+$servername = "sitedev.ipxon.net";
+$username = "tesis";
+$password = "tesis.2018";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
+
+
+
+
+
+
+</div>
