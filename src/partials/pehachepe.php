c541c3854001f6372b2bd23832f39bffb8522a1b
Guille Abella <42720588+gabella87@users.noreply.github.com>
2018-09-04 22:37:21 -0300

    1037

--- src/partials/pehachepe.php
+++ src/partials/pehachepe.php
@@ -3,12 +3,13 @@
 ?>
 <html>
     <body>
-      <p>Probando ... ...</p>
+      <p>Probando ... ... ...</p>
           <?php
                 $query = "SELECT * FROM Guille;";
                 $result = mysqli_query($db, $query);
                 while($row = mysqli_fetch_assoc($result)) {
                       // Display your datas on the page
+                      echo $row
                 }
           ?>
     </body>
